573fbda56f414dce38716962e1c4d0f842464f4a
Luiz Gustavo Chiaretto <luizgustavo@chiaretto.com.br>
2020-02-27 16:38:12 -0300

    Update flask_app.py

--- app/flask_app.py
+++ app/flask_app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
 def print_message():
-    return "You won't regret hiring me!!!"
+    return "It's me, Mario!!!"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
