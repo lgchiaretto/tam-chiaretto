638e92a646d0a2206985a681e9e1114bc83e7714
Luiz Chiaretto <luizgustavo@chiaretto.com.br>
2020-10-05 12:07:45 -0300

    added pod name

--- app/flask_app.py
+++ app/flask_app.py
@@ -1,5 +1,6 @@
 from flask import Flask
 import time
+import socket
 
 app = Flask(__name__)
 
@@ -10,6 +11,10 @@ def print_message():
 @app.route('/healthcheck', methods=['GET'])
 def print_helthcheck():
     return "ok"
+
+@app.route('/pod', methods=['GET'])
+def print_pod():
+    return socket.gethostname()
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
