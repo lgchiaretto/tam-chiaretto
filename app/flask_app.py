6573baba8f2fd597fb26ebd6971ca6258155aa7d
Luiz Chiaretto <luizgustavo@chiaretto.com.br>
2020-10-15 09:25:40 -0300

    databases

--- app/flask_app.py
+++ app/flask_app.py
@@ -1,6 +1,7 @@
 from flask import Flask
 import time
 import socket
+import mysql.connector as mysql
 
 app = Flask(__name__)
 
@@ -15,6 +16,29 @@ def print_helthcheck():
 @app.route('/pod', methods=['GET'])
 def print_pod():
     return socket.gethostname()
+
+@app.route('/databses', methods=['GET'])
+def print_databases():
+    db = mysql.connect(
+        host = "mysql-57-rhel7.chiaretto.svc",
+        user = "admin",
+        passwd = "redhat"
+    )
+
+    cursor = db.cursor()
+
+    ## executing the statement using 'execute()' method
+    cursor.execute("SHOW DATABASES")
+
+    ## 'fetchall()' method fetches all the rows from the last executed statement
+    databases = cursor.fetchall() ## it returns a list of all databases present
+
+    ## printing the list of databases
+    print(databases)
+
+    ## showing one by one database
+    for database in databases:
+        print(database)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
