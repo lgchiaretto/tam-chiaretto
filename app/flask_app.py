3e1fed93b725402e87f8ee3d23ddb20cbe3482d5
Luiz Chiaretto <luizgustavo@chiaretto.com.br>
2021-10-01 11:07:49 -0300

    lala

--- app/flask_app.py
+++ app/flask_app.py
@@ -1,66 +1,11 @@
 from flask import Flask
-from flask import jsonify
-from flask import request
-import time
-import subprocess
-import socket
-import os
-import mysql.connector as mysql
+from prometheus_flask_exporter import PrometheusMetrics
 
 app = Flask(__name__)
+metrics = PrometheusMetrics(app)
 
-@app.route('/', methods=['GET'])
-def print_message():
-    return "It's me, Mario!"
+app.debug = True
 
-@app.route('/healthcheck', methods=['GET'])
-def print_helthcheck():
-    return "ok"
-
-@app.route('/pod', methods=['GET'])
-def print_pod():
-    return socket.gethostname()
-
-@app.route('/version', methods=['GET'])
-def print_version():
-    return os.environ.get('APP_VERSION')
-
-@app.route('/env', methods=['GET'])
-def print_app_env():
-    return os.environ.get('APP_ENV')
-
-@app.route('/secret', methods=['GET'])
-def print_secret():
-    return os.environ.get('APP_SECRET')
-
-@app.route('/help', methods=['GET'])
-def print_help():
-    return """GET /
-              GET /databases
-              GET /secret
-              GET /version
-              GET /pod
-              GET /healthcheck"""
-
-@app.route('/databases', methods=['GET'])
-def print_databases():
-    db = mysql.connect(
-        host = "mysql-57-rhel7.chiaretto.svc",
-        user = "admin",
-        passwd = "redhat"
-    )
-    alldbs = []
-    cursor = db.cursor()
-    cursor.execute("SHOW DATABASES")
-    databases = cursor.fetchall()
-    for database in databases:
-        alldbs.append(database)
-    return jsonify(alldbs)
-
-@app.route('/crash', methods=['GET'])
-def ExitScript():
-    subprocess.Popen("^D")
-    return 'Server shutting down...'
-
-if __name__ == '__main__':
-    app.run(host='0.0.0.0')
+@app.route("/", methods=['GET'])
+def index():
+    return "hello world :)"
